9ef0a07a8889f099d6f7c9d2931e3785afe98453
Johnathan Normand <jnormand10@gmail.com>
2021-12-07 13:57:30 -0600

    looping employees and intital bonus

--- client.js
+++ client.js
@@ -1,34 +1,34 @@
 const employees = [
-  {
-    name: 'Atticus',
-    employeeNumber: '2405',
-    annualSalary: '47000',
-    reviewRating: 3
-  },
-  {
-    name: 'Jem',
-    employeeNumber: '62347',
-    annualSalary: '63500',
-    reviewRating: 4
-  },
-  {
-    name: 'Scout',
-    employeeNumber: '6243',
-    annualSalary: '74750',
-    reviewRating: 5
-  },
-  {
-    name: 'Robert',
-    employeeNumber: '26835',
-    annualSalary: '66000',
-    reviewRating: 1
-  },
-  {
-    name: 'Mayella',
-    employeeNumber: '89068',
-    annualSalary: '35000',
-    reviewRating: 1
-  }
+	{
+		name: 'Atticus',
+		employeeNumber: '2405',
+		annualSalary: '47000',
+		reviewRating: 3
+	},
+	{
+		name: 'Jem',
+		employeeNumber: '62347',
+		annualSalary: '63500',
+		reviewRating: 4
+	},
+	{
+		name: 'Scout',
+		employeeNumber: '6243',
+		annualSalary: '74750',
+		reviewRating: 5
+	},
+	{
+		name: 'Robert',
+		employeeNumber: '26835',
+		annualSalary: '66000',
+		reviewRating: 1
+	},
+	{
+		name: 'Mayella',
+		employeeNumber: '89068',
+		annualSalary: '35000',
+		reviewRating: 1
+	}
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
@@ -39,4 +39,38 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
+
+
+//function loopEmployee(array){
+
+//}
+//loopEmployee(employees);
+
+
+
+function employeeBonus(array){
+
+  for(let i = 0; i < array.length; i++){
+    console.log(array[i]);
+console.log(employees);
+
+  if(employees[i].reviewRating === 3){
+    employees[i].bonus = 4;
+  }
+  else if(employees[i].reviewRating === 4){
+    employees[i].bonus = 6;
+  }
+  else if(employees[i].reviewRating === 5){
+    employees[i].bonus = 10;
+  }
+  else{
+    employees[i].bonus = 0;
+  }
+}
+}
+
+
+employeeBonus(employees);
+
+console.log(employees);
